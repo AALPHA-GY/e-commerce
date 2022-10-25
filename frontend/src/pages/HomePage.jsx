74251478e14fd4acbf1e47f5dc20bd938c273e87
Batuhan Yegin <batuhanyegin23@gmail.com>
2022-10-25 18:41:33 +0300

    tüm eklentiler yüklendi ve site tasarlanmasına başlanıldı

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+ export class HomePage extends Component {
+  render() {
+    return (
+     <Fragment>
+        <h1>Anasayfamız</h1>
+     </Fragment>
+    )
+  }
+}
+
+export default HomePage
